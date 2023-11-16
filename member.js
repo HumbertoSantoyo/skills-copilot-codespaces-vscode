8e434a57a9cb16c2d858771dddc465ace7ddfda9
Humberto Santoyo Ruiz <80534265+HumbertoSantoyo@users.noreply.github.com>
2023-11-16 15:44:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkills2 = document.getElementById("memberSkills2");
+    var memberSkills3 = document.getElementById("memberSkills3");
+    var memberSkills4 = document.getElementById("memberSkills4");
+    var memberSkills5 = document.getElementById("memberSkills5");
+    var memberSkills6 = document.getElementById("memberSkills6");
+    var memberSkills7 = document.getElementById("memberSkills7");
+    var memberSkills8 = document.getElementById("memberSkills8");
+    var memberSkills9 = document.getElementById("memberSkills9");
+    var memberSkills10 = document.getElementById("memberSkills10");
+    var memberSkills11 = document.getElementById("memberSkills11");
+    var memberSkills12 = document.getElementById("memberSkills12");
+    var memberSkills13 = document.getElementById("memberSkills13");
+    var memberSkills14 = document.getElementById("memberSkills14");
+    var memberSkills15 = document.getElementById("memberSkills15");
+    var memberSkills16 = document
